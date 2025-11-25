3510d1e1a7d63ef7146eac0094491c593cc0cfcb
TOM-BOSS-pinetour <b94889340@gmail.com>
2025-11-26 01:55:33 +0800

    Formik Yup addedd to addNewFoodCard

--- src/app/_components/AddNewFood.js
+++ src/app/_components/AddNewFood.js
@@ -2,25 +2,18 @@
 
 import { useState } from "react";
 import PlusIcon from "../_icons/plusIcon";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 export default function AddNewFoodCard() {
   const [open, setOpen] = useState(false);
 
-  const handleSubmit = async () => {
-    try {
-      await axios.post("http://localhost:1000/food", {
-        foodName: foodName,
-        foodPrice: foodPrice,
-        foodIngredients: foodIngredients,
-        foodImage: foodImage,
-        category: category,
-      });
-      setOpen(false);
-      setCategoryName("");
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const foodSchema = Yup.object().shape({
+    foodName: Yup.string().required("Food Name Required!"),
+    foodPrice: Yup.string().required("Food Price Required!"),
+    foodIngredients: Yup.string().required("Ingredients is Required!"),
+  });
 
   return (
     <div className="w-[270.75px] h-[241px] border rounded-xl border-red-500 flex flex-col justify-center items-center gap-6">
@@ -34,12 +27,17 @@ export default function AddNewFoodCard() {
         Add new Dish to Appetizers
       </div>
       {open && (
-        <div className=" fixed inset-0 bg-black/40 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-xl shadow-xl w-[472px] h-[596px] flex flex-col gap-6 relative justify-between">
+        <Formik
+          initialValues={{ foodName: "", foodPrice: "", foodIngredients: "" }}
+          validateOnMount={foodSchema}
+          onSubmit={(values) => {
+            console.log("Form Data", values);
+          }}
+          className=" fixed inset-0 bg-black/40 flex justify-center items-center"
+        >
+          <Form className="bg-white p-6 rounded-xl shadow-xl w-[472px] h-[596px] flex flex-col gap-6 relative justify-between">
             <div className="flex justify-between items-center">
-              <h2 className="text-xl font-semibold">
-                Add new Dish to Appetizers
-              </h2>
+              <h2 className="text-xl font-semibold">Add new Dish to</h2>
               <button
                 onClick={() => setOpen(false)}
                 className="text-black text-xl hover:text-black w-9 h-9 rounded-full bg-[#F4F4F5]"
@@ -50,28 +48,43 @@ export default function AddNewFoodCard() {
             <div className="flex flex-row gap-6">
               <div className="flex flex-col gap-2">
                 <div className="text-[14px]">Food name</div>
-                <input
+                <Field
+                  name="foodName"
                   placeholder="Type food name"
-                  type="text"
-                  className="border p-2 rounded-md h-[38px]"
-                ></input>
+                  className="border rounded-md h-[38px] p-2 "
+                />
+                <ErrorMessage
+                  name="foodName"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
               </div>
               <div className="flex flex-col gap-2">
                 <div className="text-[14px]">Food price</div>
-                <input
+                <Field
+                  name="foodPrice"
                   placeholder="Enter price"
-                  type="text"
-                  className="border p-2 rounded-md h-[38px]"
-                ></input>
+                  className="border rounded-md h-[38px] p-2"
+                />
+                <ErrorMessage
+                  name="foodPrice"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
               </div>
             </div>
             <div className="flex flex-col gap-2">
               <div>Ingredients</div>
-              <input
-                className="border p-2 rounded-md h-[90px] w-full"
+              <Field
+                name="foodIngredients"
                 placeholder="List ingredients..."
-                type="text"
-              ></input>
+                className="border rounded-md h-[38px] p-2"
+              />
+              <ErrorMessage
+                name="foodIngredients"
+                component="div"
+                className="text-red-500 text-sm"
+              />
             </div>
             <div className="flex flex-col gap-2">
               <div className="text-[14px] font">Food image</div>
@@ -82,16 +95,16 @@ export default function AddNewFoodCard() {
               />
             </div>
 
-            <div className="flex justify-end gap-3">
+            <div className="flex justify-end gap-3 w-full h-[64px] items-end">
               <button
-                onClick={handleSubmit}
+                type="submit"
                 className="px-4 py-2 rounded-md bg-black text-white"
               >
                 Add Dish
               </button>
             </div>
-          </div>
-        </div>
+          </Form>
+        </Formik>
       )}
     </div>
   );
