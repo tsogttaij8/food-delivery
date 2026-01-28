bfba0189a9cabe9901d88c86cff90887801b6609
25LP7998 25LP7998 <25LP7998@G302-CC0115.local>
2026-01-28 17:13:24 +0800

    test

--- src/app/_constants/index.js
+++ src/app/_constants/index.js
@@ -1,2 +1,2 @@
-export const BACK_END_URL = "http://localhost:1000";
-// export const BACK_END_URL = "";
+// export const BACK_END_URL = "http://localhost:1000";
+export const BACK_END_URL = "https://food-delivery-back-end-maad.onrender.com";
